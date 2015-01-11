7be277d5293862df59889135428b77b67a214795
Pablo de la Peña <p@shedcollective.org>
2015-01-11 21:36:18 +0000

    Migration update

--- migrations/0.sql
+++ migrations/0.sql
@@ -1,4 +1,4 @@
 # INITIAL ADMIN DB
 # This is the schema of the event module database as of 09/01/2015
-DROP TABLE IF EXISTS `nails_event`;
-CREATE TABLE `nails_event` (`id` int(11) unsigned NOT NULL AUTO_INCREMENT, `type` varchar(50) NOT NULL DEFAULT '', `url` varchar(300) DEFAULT NULL, `data` text, `ref` int(11) unsigned DEFAULT NULL, `created` datetime NOT NULL, `created_by` int(11) unsigned DEFAULT NULL, PRIMARY KEY (`id`), KEY `created_by` (`created_by`), CONSTRAINT `nails_event_ibfk_2` FOREIGN KEY (`created_by`) REFERENCES `nails_user` (`id`) ON DELETE CASCADE) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `{{NAILS_DB_PREFIX}}event`;
+CREATE TABLE `{{NAILS_DB_PREFIX}}event` (`id` int(11) unsigned NOT NULL AUTO_INCREMENT, `type` varchar(50) NOT NULL DEFAULT '', `url` varchar(300) DEFAULT NULL, `data` text, `ref` int(11) unsigned DEFAULT NULL, `created` datetime NOT NULL, `created_by` int(11) unsigned DEFAULT NULL, PRIMARY KEY (`id`), KEY `created_by` (`created_by`), CONSTRAINT `{{NAILS_DB_PREFIX}}event_ibfk_2` FOREIGN KEY (`created_by`) REFERENCES `{{NAILS_DB_PREFIX}}user` (`id`) ON DELETE CASCADE) ENGINE=InnoDB DEFAULT CHARSET=utf8;
