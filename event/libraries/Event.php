d8873d7bba5da082c59cde85c5bf1ed491449aa3
Pablo de la Pena <p@shedcollective.org>
2015-01-27 15:14:37 +0000

    Doc updates

--- event/libraries/Event.php
+++ event/libraries/Event.php
@@ -136,14 +136,12 @@ class Event
 
     /**
      * Creates an event object
-     * @param  string  $type               The type of event to create
-     * @param  mixed   $data               Any data to store alongside the event object
-     * @param  integer $created_by         The event creator (NULL == system)
-     * @param  integer $ref                A numeric reference to store alongside the event (e.g the id of the object
-     *                                     the event relates to)
-     * @param  string  $recorded           A strtotime() friendly string of the date to use instead of NOW() for the
-     *                                     created date
-     * @return mixed                       Int on success FALSE on failure
+     * @param  string  $type      The type of event to create
+     * @param  mixed   $data      Any data to store alongside the event object
+     * @param  integer $createdBy The event creator (null == system)
+     * @param  integer $ref       A numeric reference to store alongside the event (e.g the id of the object the event relates to)
+     * @param  string  $recorded  A strtotime() friendly string of the date to use instead of NOW() for the created date
+     * @return mixed              Int on success false on failure
      */
     public function create($type, $data = null, $createdBy = null, $ref = null, $recorded = null)
     {
