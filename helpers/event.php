27f336c1b78a1ffd1d692f4f87487b0f0c385a70
Pablo de la Peña <p@shedcollective.org>
2015-11-24 18:23:12 +0000

    Added helper (from `common`)

---
+++ helpers/event.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * This file provides event related helper functions
+ *
+ * @package     Nails
+ * @subpackage  module-event
+ * @category    Helper
+ * @author      Nails Dev Team
+ * @link
+ */
+
+if (!function_exists('create_event')) {
+
+    /**
+     * Creates an event object
+     * @param  string  $type      The type of event to create
+     * @param  mixed   $data      Any data to store alongside the event object
+     * @param  integer $createdBy The event creator (null == system)
+     * @param  integer $ref       A numeric reference to store alongside the event (e.g the id of the object the event relates to)
+     * @param  string  $recorded  A strtotime() friendly string of the date to use instead of NOW() for the created date
+     * @return mixed              Int on success false on failure
+     */
+    function create_event($type, $data = null, $createdBy = null, $ref = null, $recorded = null)
+    {
+        $ci =& get_instance();
+        return $ci->event->create($type, $data, $createdBy, $ref, $recorded);
+    }
+}
