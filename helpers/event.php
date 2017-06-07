2de519506b84c28193b4a5c85a76a50759150b29
Pablo de la Peña <p@shedcollective.org>
2017-06-07 11:17:32 +0100

    Using Factory in event helper

--- helpers/event.php
+++ helpers/event.php
@@ -1,5 +1,7 @@
 <?php
 
+use Nails\Factory;
+
 /**
  * This file provides event related helper functions
  *
@@ -14,16 +16,18 @@ if (!function_exists('create_event')) {
 
     /**
      * Creates an event object
-     * @param  string  $type      The type of event to create
-     * @param  mixed   $data      Any data to store alongside the event object
-     * @param  integer $createdBy The event creator (null == system)
-     * @param  integer $ref       A numeric reference to store alongside the event (e.g the id of the object the event relates to)
-     * @param  string  $recorded  A strtotime() friendly string of the date to use instead of NOW() for the created date
+     *
+     * @param  string  $sType      The type of event to create
+     * @param  mixed   $aData      Any data to store alongside the event object
+     * @param  integer $iCreatedBy The event creator (null == system)
+     * @param  integer $iRef       A numeric reference to store alongside the event (e.g the ID of the object the event relates to)
+     * @param  string  $sRecorded  A strtotime() friendly string of the date to use instead of NOW() for the created date
+     *
      * @return mixed              Int on success false on failure
      */
-    function create_event($type, $data = null, $createdBy = null, $ref = null, $recorded = null)
+    function create_event($sType, $aData = null, $iCreatedBy = null, $iRef = null, $sRecorded = null)
     {
-        $ci =& get_instance();
-        return $ci->event->create($type, $data, $createdBy, $ref, $recorded);
+        $oEvent = Factory::service('Event', 'nailsapp/module-event');
+        return $oEvent->create($sType, $aData, $iCreatedBy, $iRef, $sRecorded);
     }
 }
